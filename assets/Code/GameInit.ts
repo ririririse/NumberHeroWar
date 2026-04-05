9fe1386decafa3dc9e899af24f19c1656d649d2b
USagi <2959524294@qq.com>
2026-04-05 17:14:38 +0800

    解决了怪物和宝箱分布聚集，并实现了不同怪物外观

--- assets/Code/GameInit.ts
+++ assets/Code/GameInit.ts
@@ -3,10 +3,10 @@ const { ccclass } = _decorator;
 
 @ccclass('GameInit')
 export class GameInit extends Component {
-    onLoad() {
-        // 开启物理系统
-        PhysicsSystem2D.instance.enable = true;
-        // 调试用：开启下面这行可以看到绿色的碰撞框，正式发布请注释掉
-         PhysicsSystem2D.instance.debugDrawFlags = EPhysics2DDrawFlags.All;
-    }
+    onLoad() {
+        // 开启物理系统
+        PhysicsSystem2D.instance.enable = true;
+        // 调试用：开启下面这行可以看到绿色的碰撞框，正式发布请注释掉
+        //PhysicsSystem2D.instance.debugDrawFlags = EPhysics2DDrawFlags.All;
+    }
 }
